a8ef36ed9c749ddc81bd5e2714aaad473a47ba61
Spectre-SX <miki.robox@gmail.com>
2025-06-08 14:44:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ScrambledText from "./blocks/TextAnimations/ScrambledText/ScrambledText.j
 import ScrollReveal from "./blocks/TextAnimations/ScrollReveal/ScrollReveal.jsx";
 import Dock from "./blocks/Components/Dock/Dock.jsx";
 import SpotlightCard from "./blocks/Components/SpotlightCard/SpotlightCard.jsx";
-import ProfileCard from 'src/blocks/components/ProfileCard/ProfileCard.jsx';
+import ProfileCard from './blocks/Components/ProfileCard/ProfileCard.jsx';
 import Iridescence from "./blocks/Backgrounds/Iridescence/Iridescence.jsx";
 import { VscHome, VscArchive, VscAccount, VscSettingsGear } from "react-icons/vsc";
 import { FaGithub, FaDiscord, FaLinkedin, FaGlobe } from "react-icons/fa";
