44f6b94e648e12df4ffe508ced04b3a247b283df
Spectre-SX <miki.robox@gmail.com>
2025-06-08 17:22:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import Home from "./Home.jsx";     // <- Your Home component
-import NotFound from "./404/404.jsx";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./Home.jsx";
+import NotFound from "./404.jsx"; // your 404 page component file
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/404" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/404" element={<NotFound />} />
+
+      {/* Redirect root to home */}
+      <Route path="/" element={<Navigate to="/home" replace />} />
+
+      {/* Catch all unmatched routes and show 404 */}
+      <Route path="*" element={<Navigate to="/404" replace />} />
+    </Routes>
   );
 }
